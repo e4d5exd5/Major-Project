aedec6b54b5381952b6481a7191d6d95305633a0
AdityaSawant0912 <arsawant09@gmail.com>
2023-12-21 12:44:15 +0530

    new testing changes and mjority voting

--- lib/Predict.py
+++ lib/Predict.py
@@ -11,6 +11,20 @@ import numpy as np
 import random
 from tqdm.auto import tqdm
 
+def generateSupportSet(patches: list, imageData: tuple, C, K):
+    
+    (IMAGE_HEIGHT, IMAGE_WIDTH, IMAGE_DEPTH, IMAGE_CHANNEL) = imageData
+    
+    supportPatches = []
+    supportLabels = [i for i in range(1, C+1)]
+    for n in supportLabels:
+        sran_indices = np.random.choice(len(patches[n-1]),K,replace=False)  # for class no X-1: select K samples 
+        supportPatches.extend( patches[n-1][sran_indices,:,:,:,:])
+    supportPatches = tf.convert_to_tensor(np.reshape(np.asarray(supportPatches),(C*K,IMAGE_HEIGHT,IMAGE_WIDTH,IMAGE_DEPTH,IMAGE_CHANNEL)),dtype=tf.float32)
+    
+    
+    return supportPatches, supportLabels
+
 
 def generateWindow(X: np.ndarray, Y: np.ndarray, patches: list, windowSize: int, imageData: tuple, C, K):
 
@@ -21,25 +35,18 @@ def generateWindow(X: np.ndarray, Y: np.ndarray, patches: list, windowSize: int,
             
             queryPatches = X[x:x+windowSize, y:y+windowSize, :, :, :]
             queryLabels = Y[x:x+windowSize, y:y+windowSize]
-            supportPatches = []
-            supportLabels = [i for i in range(1, C+1)]
-            for n in supportLabels:
-                sran_indices = np.random.choice(len(patches[n-1]),K,replace=False)  # for class no X-1: select K samples 
-                supportPatches.extend( patches[n-1][sran_indices,:,:,:,:])
-            supportPatches = tf.convert_to_tensor(np.reshape(np.asarray(supportPatches),(C*K,IMAGE_HEIGHT,IMAGE_WIDTH,IMAGE_DEPTH,IMAGE_CHANNEL)),dtype=tf.float32)
-    
 
             w = len(queryLabels)
             h = len(queryLabels[0])
             queryLabels = np.reshape(np.asarray(queryLabels), (len(queryLabels) * len(queryLabels[0])))
             queryPatches = tf.convert_to_tensor(np.reshape(np.asarray(queryPatches),(len(queryLabels),IMAGE_HEIGHT,IMAGE_WIDTH,IMAGE_DEPTH,IMAGE_CHANNEL)),dtype=tf.float32)
-            yield x, y, queryLabels.tolist(), queryPatches, supportLabels, supportPatches ,w, h
+            yield x, y, queryLabels.tolist(), queryPatches, w, h
 
 # 0 1 2 3 4 5 6 7 8 9 10 11
 
 
 
-def predictImage(Data: Data, ProtoModel: Prototypical , imageData: tuple, N_TIMES, windowSize):
+def predictImage(Data: Data, ProtoModel: Prototypical , imageData: tuple, N_TIMES, windowSize, VotingTimes=5, RandomSupport=True):
     (IMAGE_HEIGHT,IMAGE_WIDTH,IMAGE_DEPTH,IMAGE_CHANNEL) = imageData
     X, Y = Data.createPatches()
     _, _, X_patchwise = Data.get_data()
@@ -48,20 +55,33 @@ def predictImage(Data: Data, ProtoModel: Prototypical , imageData: tuple, N_TIME
     predictions = np.zeros((X_shape[0], X_shape[1]))
     predictionsTrue = np.zeros((X_shape[0], X_shape[1]))
     K = 5
-    all_preds = []
-    all_y_preds = []
+
     # print(predictions.shape)
             
     
 
     epochObj = tqdm(generateWindow(X, Y, X_patchwise, windowSize, imageData, C, K), desc=f'Patch', total=((X.shape[0]//windowSize) * (X.shape[1] //windowSize)))
-    for x, y, queryLabels, queryPatches, supportLabels, supportPatches, w, h in epochObj:
+    for x, y, queryLabels, queryPatches, w, h in epochObj:
         # print(x, y, len(queryLabels), queryPatches.shape, len(supportLabels), supportPatches.shape)
-        loss, mean_predictions, mean_accuracy, classwise_mean_acc, y_preds = ProtoModel(supportPatches, queryPatches, supportLabels, queryLabels, K, C, len(queryLabels), training=False)
-        correctIndices = tf.cast(tf.argmax(mean_predictions, axis=-1) + 1, tf.int32) 
-        predictions[x:x+w,  y:y+h] = np.reshape(correctIndices.numpy(), (w, h))
-        all_preds.extend(mean_predictions)
-        all_y_preds.extend(y_preds)
+        supportPatches, supportLabels = generateSupportSet(X_patchwise, imageData, C, K)
+        
+        votes = []
+        
+        for i in range(VotingTimes):
+            loss, mean_predictions, mean_accuracy, classwise_mean_acc, y_preds = ProtoModel(supportPatches, queryPatches, supportLabels, queryLabels, K, C, len(queryLabels), training=False)
+            correctIndices = tf.cast(tf.argmax(mean_predictions, axis=-1) + 1, tf.int32) 
+            votes.append(correctIndices)
+            if(RandomSupport):
+                supportPatches, supportLabels = generateSupportSet(X_patchwise, imageData, C, K)
+        
+        # Calulate Majority Voting Here
+        votes_tensor = tf.stack(votes, axis=-1)  # Stack the votes along the last axis
+        majority_votes, _ = tf.unique_with_counts(tf.reshape(votes_tensor, [-1]))  # Count occurrences of each class
+        majority_class = tf.argmax(majority_votes) + 1  # Get the class with the maximum count
+
+        # Fill in the predictions with the majority voting result
+        predictions[x:x+w, y:y+h] = majority_class.numpy()
+       
     
     y_test = []
     y_pred = []
